68905efa17c80c8311c551ffd570bb7f6efde646
Multicellular Organism <theaustinator007@gmail.com>
2020-12-15 12:14:00 -0800

    f

--- JS/Themery.js
+++ JS/Themery.js
@@ -6,16 +6,16 @@ function setTheme(themeName) {
 // function to toggle between light and dark theme
 function toggleTheme() {
    if (localStorage.getItem('theme') === 'theme-plain-dark'){
-       setTheme('theme-plain-light');
-   } else {
        setTheme('theme-plain-dark');
+   } else {
+       setTheme('theme-plain-light');
    }
 }
 // Immediately invoked function to set the theme on initial load
 (function () {
    if (localStorage.getItem('theme') === 'theme-plain-light') {
-       setTheme('theme-plain-dark');
-   } else {
        setTheme('theme-plain-light');
+   } else {
+       setTheme('theme-plain-dark');
    }
 })();
